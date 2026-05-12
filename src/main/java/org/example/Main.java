2926e8fc889907998754b8f3ba733fe08719ddfc
Guillermo Lainez <lainezotamendi@gmail.com>
2026-05-12 22:58:23 +0200

    feat: incorpora búsqueda de productos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,27 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("MiniInventario iniciado correctamente.");
         System.out.println("Sistema preparado para futuras mejoras.");
+    }
 
+    public static void buscarProducto(ArrayList<String> productos, String nombreBuscado) {
+        boolean encontrado = false;
+
+        for (String producto : productos) {
+            if (producto.equalsIgnoreCase(nombreBuscado)) {
+                encontrado = true;
+                break;
+            }
+        }
+
+        if (encontrado) {
+            System.out.println("Producto encontrado en el inventario.");
+        } else {
+            System.out.println("El producto no existe en el inventario.");
+        }
     }
 }
