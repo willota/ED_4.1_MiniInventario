aacca5b059718fbfccf2a6e74ff160ac0470ce4e
Guillermo Lainez <lainezotamendi@gmail.com>
2026-05-12 22:43:26 +0200

    feat: añade mensaje de mejora futura

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("MiniInventario iniciado correctamente.");
+        System.out.println("Sistema preparado para futuras mejoras.");
 
     }
 }
